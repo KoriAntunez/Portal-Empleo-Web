fc110e61bde3de2650412da796e906c7fe25e048
S3AN2 <99769699+S3AN2@users.noreply.github.com>
2022-03-22 23:31:20 -0500

    nuevo_servicio

---
+++ portal_Empleo/js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("file").onchange=function(e){
+let reader =new FileReader();
+reader.readAsDataURL(e.target.files[0]);
+reader.onload = function() {
+let preview = document.getElementById("preview");
+imagen = document.createElement("img");
+imagen.src =reader.result;
+imagen.style.width="327px";
+preview.innerHTML="";
+preview.append(imagen);
+}
+}
